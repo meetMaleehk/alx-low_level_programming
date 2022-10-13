ea8af32806610a33ac802047c7bb485005230142
Godwin Victor Malik <maleehkvictor@gmail.com>
2022-10-13 05:45:03 -0700

    C Level Programming

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,13 +1,20 @@
 #include <stdio.h>
 /**
- * main - Entry
- * Description: prints "with proper grammar, but the outcome
- *is a piece of art,"
- * Return: Always 0
+ * main - Prints sized of characters
+ * Return: Always 0 (success)
  */
 int main(void)
 {
-	printf("with proper grammar, but the outcome
-is a piece of art,\n");
+
+	printf("Size of a char: %zu byte(s)\n", sizeof(char));
+
+	printf("Size of an int: %zu byte(s)\n", sizeof(int));
+
+	printf("Size of a long int: %zu byte(s)\n", sizeof(long int));
+
+	printf("Size of a long long int: %zu byte(s)\n", sizeof(long long int));
+
+	printf("Size of a float: %zu byte(s)\n", sizeof(float));
 	return (0);
+
 }
