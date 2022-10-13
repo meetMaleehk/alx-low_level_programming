25c167f52bc7aed5bfba497b4d1db566fc960b94
Godwin Victor Malik <maleehkvictor@gmail.com>
2022-10-13 03:02:44 -0700

    C Level programming

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -4,15 +4,22 @@
 
 /**
  *
- *   main - prints the phrase "Programming is like building a multilingual
- *  puzzle
+ *  * main - Entry point
  *
- *   * Return: 0 if exited properly, non-zero otherwise
+ *   *
  *
+ *    * Description: A C program that prints with put function
+ *
+ *     *
+ *
+ *      * Return: Always 0 (Success)
+ *
+ *      */
 
- */
+
 
 int main(void)
+
 {
 
 		puts("\"Programming is like building a multilingual puzzle");
