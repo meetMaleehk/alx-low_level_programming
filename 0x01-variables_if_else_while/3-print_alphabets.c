4f01dc2e31abe8841b444f1ea8bcdee4f3d7a13f
meetMaleehk <maleehkvictor@gmail.com>
2022-10-16 14:41:58 +0100

    C is Fun

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,33 +1,21 @@
-#include <stdio.h> 
-  
- /** 
-  * main - print the letters of the alphabe 
-  * 
-  * Description: print the letters of the aphabet in lower then in uppercase 
-  * 
-  * Return: Always 0 (Success) 
-  */ 
-  
- int main(void) 
- { 
-         int i, j; 
-  
-         i = 97; 
-         j = 65; 
-  
-         while (i < 123) 
-         { 
-                 putchar(i); 
-                 i++; 
-         } 
-  
-         while (j < 91) 
-         { 
-                 putchar(j); 
-                 j++; 
-         } 
-  
-         putchar(10); 
-  
-         return (0); 
- }
+#include <stdio.h>
+/**
+ * main - Entry Point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char c, d;
+
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		putchar(c);
+	}
+	for (d = 'A'; d <= 'Z'; d++)
+	{
+		putchar(d);
+	}
+	putchar(10);
+	return (0);
+}
