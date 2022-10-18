c653d9af3f7398f9806a6802c798881125aa331f
meetMaleehk <maleehkvictor@gmail.com>
2022-10-18 10:25:22 +0100

    C is Fun

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * main - Entry Point
+ *
+ * Return: Always 0 (Success)
+ **/
+int main(void)
+{
+	char a[] = "_putchar";
+
+	int c;
+
+	for (c = 0; c < 8; c++)
+	{
+		_putchar(a[c]);
+	}
+	_putchar('\n');
+	return (0);
+}
