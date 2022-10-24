6af136ef5153871aa9067d011c31c1a24e2a7905
Godwin Victor Malik <maleehkvictor@gmail.com>
2022-10-24 12:28:43 -0700

    C isn't Fair

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -1,17 +1,27 @@
 #include "main.h"
+#include <stdio.h>
+
 /**
- * char *_strcpy - a function that copies the string pointed to by src
- * @dest: copy to
- * @src: copy from
- * Return: string
+ * print_array - Entry point after main
+ * Description: Prints n elements of an array of integers
+ * followed by a new line.
+ * Author: Jonathan Boomni
+ * @a: integer array passed
+ * @n: number of elements in array to be printed
+ * Return: 0
  */
-char *_strcpy(char *dest, char *src)
+void print_array(int *a, int n)
 {
-int i = 0;
-for (; src[i] != '\0'; i++)
-{
-dest[i] = src[i];
-}
-dest[i] = '\0';
-return (dest);
+	int i;
+
+	i = 0;
+	for (n--; n >= 0; n--, i++)
+	{
+		printf("%d", a[i]);
+		if (n > 0)
+		{
+			printf(", ");
+		}
+	}
+	printf("\n");
 }
