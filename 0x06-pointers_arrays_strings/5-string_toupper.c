37ea713a49a38b8d6e9dc9e50d7656f59b94ed6b
Godwin Victor Malik <maleehkvictor@gmail.com>
2022-10-29 02:04:39 -0700

    Hot Fix

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * string_toupper -  function that changes all lowercase letter
+ * of a string to uppercase.
+ *@n: cadena de caracteres.
+ * Return: n.
+ */
+
+char *string_toupper(char *n)
+{
+	int i;
+
+	i = 0;
+	while (n[i] != '\0')
+	{
+
